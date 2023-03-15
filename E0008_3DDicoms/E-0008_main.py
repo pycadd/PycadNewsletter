07b4cd675411a99cfcd6d33ac9fc2aa809fb94a5
amine0110 <mokhtari.amine1996@gmail.com>
2023-03-15 21:52:37 +0100

    8th edition - 3D Dicoms

--- E0008_3DDicoms/E-0008_main.py
+++ E0008_3DDicoms/E-0008_main.py
@@ -5,4 +5,4 @@ dicom_array = dicom_file.pixel_array
 number_of_frames = dicom_file.NumberOfFrames
 
 print('Shape:', dicom_array.shape)
-print('Number of frames:', number_of_frames)
+print('Number of frames:', number_of_frames) 
