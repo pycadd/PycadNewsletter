b42adda2a1047c6475f2d6cfe4d1127f2daea51e
amine0110 <mokhtari.amine1996@gmail.com>
2023-03-15 21:53:33 +0100

    7th edition - ITK for smoothening

--- E0007_Smoothening/E-0007_smoothening.py
+++ E0007_Smoothening/E-0007_smoothening.py
@@ -7,4 +7,4 @@ input_image = itk.imread("input_image.nii.gz") # or input_image.nii
 smoothed_image = itk.median_image_filter(input_image, redius=2) # More you incread the `redius` more the image will be smooth
 
 #Save the result
-itk.imwrite(smoothed_image, "output_image.nii.gz") # or output_image.nii
+itk.imwrite(smoothed_image, "output_image.nii.gz") # or output_image.nii 
